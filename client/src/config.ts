3a7a5b74b237ea4c1c2be2525f74f27cb56a76e4
Avakash D <avakashd@avakashd-a01.vmware.com>
2021-06-10 18:39:23 +0530

    Updated

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '2ltfupwzgj'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'avakashd.us.auth0.com',           // Auth0 domain
+  clientId: 'yLnBaFGdN6IK99pjNBzumOCtHlOGVsuV',  // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
